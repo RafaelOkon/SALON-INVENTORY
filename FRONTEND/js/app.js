eea3377549ae6617c92590065a149d0714e5e2d7
RafaelOkon <raphaelbazy@gmail.com>
2026-08-10 22:27:41 +0100

    feat: create responsive login page

--- FRONTEND/js/app.js
+++ FRONTEND/js/app.js
@@ -1,1 +1,28 @@
-console.log("Salon Inventory Management System");
+// =========================
+// PASSWORD VISIBILITY
+// =========================
+
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+if (togglePassword && password) {
+
+    togglePassword.addEventListener("click", function () {
+
+        const isPassword = password.type === "password";
+
+        password.type = isPassword ? "text" : "password";
+
+        const icon = togglePassword.querySelector("i");
+
+        if (isPassword) {
+            icon.classList.remove("bi-eye");
+            icon.classList.add("bi-eye-slash");
+        } else {
+            icon.classList.remove("bi-eye-slash");
+            icon.classList.add("bi-eye");
+        }
+
+    });
+
+}
